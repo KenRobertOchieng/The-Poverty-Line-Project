cdc5b1186d63413306a61c69a61aa3e5d1486162
Klein <klein.gachuhi@student.moringaschool.com>
2025-07-16 07:09:23 +0300

    Structured imports between Login Form, Page and App jsx files

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import LoginForm from '../components/LoginForm';
 function LoginPage() {
   return (
-    <div>LoginPage</div>
-  )
+    <div className="login-container">
+      <div className="overlay"></div>
+      <div className="login-box">
+        <LoginForm />
+      </div>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
