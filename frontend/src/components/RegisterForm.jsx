12ca0afef65629d45bda59410f4bad09f2238975
Lisa Alusa <sayilisaalusa@gmail.com>
2025-07-16 07:41:53 +0300

    added some comments and working on validation section

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -1,3 +1,26 @@
 import React, { useState } from 'react';
 
-const RegisterForm = ({})
+const RegisterForm = ({onSubmit, loading, error, success}) => {
+    // Local form state
+    const [formData, setFormData] = useState({
+        fullName: '',
+        email: '',
+        username: '',
+        password: '',
+        confirmPassword: '',
+    });
+
+    // Handle input changes
+    const handleChange = (e) => {
+        const { name, value} = e.target;
+        setFormData((prev) => ({ ...prev, [name]: value}));
+    };
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //Client-side validation
+    if (formData.password !== formData.confirmPassword){
+        alert("Passwords do not match")
+    }
+}
