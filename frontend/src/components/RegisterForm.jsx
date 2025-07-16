60ea06d4b157429d1fd4264be2ee259832d931f7
Lisa Alusa <sayilisaalusa@gmail.com>
2025-07-16 22:27:52 +0300

    make changes

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -35,34 +35,6 @@ const handleSubmit = (e) => {
 };
 
 return (
-    <form 
-    onSubmit = {handleSubmit}
-    className="bg-white p-6 rounded-lg shadow-md space-y-4 max-w-md mx-auto mt-10"
->
-    <h2 className = "text-2xl font-bold text-green-700 text-center">Create Account</h2>
-    {/*Feedback messages*/}
-    {error && <p className="text-red-500 text-sm text-center">{error}</p>}
-    {success && <p className="text-green-500 text-sm text-center">Registration successful!</p>}
-
-    {/*Form inputs*/}
-    <input
-    type = "text"
-    name = "fullName"
-    placeholder="Full Name"
-    value={formData.fullName}
-    onChange={handleChange}
-    required
-    className="w-full px-4 py-2 border rounded-md"
-    />
-    <input
-    type = "email"
-    name = "email"
-    placeholder="Email"
-    value={formData.email}
-    onChange={handleChange}
-    required
-    className="w-full px-4 py-2 border rounded-md"
-    />
-   </form> 
-);
+    <div className="min-h-screen flex items-center justify-center"
+)
 };
