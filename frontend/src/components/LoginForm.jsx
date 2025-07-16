f358a5bd5bbebbc7abb0451370bcb4b2a0af4674
Klein <klein.gachuhi@student.moringaschool.com>
2025-07-16 07:21:00 +0300

    Input for username or email address

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function LoginForm() {
   return (
-    <div>LoginForm</div>
+    <div className='login-container'>
+        <div className='login-header'>
+            <h2>Poverty Line</h2>
+            <p className='welcoming-text'> A step towards eradicating poverty</p>
+            <div className='divider-container'>
+                <div className='divider-line' ></div>
+            </div>
+        </div>
+        <form className='login-form'>
+            <div className='form-group'>
+                <label htmlFor="username or email address" className='form-label'>Username or email address</label>
+                <input type="text" id='username or email'  name='username or email' placeholder='Enter username or email' className='form-input'/>
+                    </div>
+        </form>
+    </div>
   )
 }
 
