7c996632f5fdffcbe586e71737d64f919a8d1a4a
Lisa Alusa <sayilisaalusa@gmail.com>
2025-07-18 09:32:36 +0300

    working on navigation bar

--- frontend/src/pages/RestigerPage.jsx
+++ frontend/src/pages/RestigerPage.jsx
@@ -1,1 +1,20 @@
-import React from
+import React from 'react'
+import RegisterForm from '../components/RegisterForm';
+
+const RegisterPage = () => {
+    return (
+        <div className="min-h-screen flex flex-col bg-gradient-to-br from-green-100 to-blue-100">
+
+            {/*Navigation Bar */}
+            <nav className="bg-white shadow-md py-4 px-8 flex justify-between items-center">
+                <ul className="flex space-x-6 text-green-900 font-medium">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/dashboard">Dashboard</a></li>
+
+                    <li><a href="/login">Login</a></li>
+                    <li></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
