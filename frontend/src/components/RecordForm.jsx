1a38068b0546649d339f32b64fd97f102e9cf822
Lisa Alusa <sayilisaalusa@gmail.com>
2025-07-18 17:38:26 +0300

    working on the input changes

--- frontend/src/components/RecordForm.jsx
+++ frontend/src/components/RecordForm.jsx
@@ -2,3 +2,23 @@ import Rect, { useState, useEffect } from 'react';
 import { useDispatch, useSelector} from 'react-redux';
 import { addRecord, updateRecord} from '../features/records/recordSlice';
 
+const RecordForm = ({ editMode = false, recordToEdit}) => {
+    const dispatch = useDispatch();
+
+    // local state for form data
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        phone: '',
+    });
+    // when a record is chosen, update the form fields
+    useEffect(() => {
+        if (selectedRecord){
+            setFormData(selectedRecord);
+        }
+
+    }, [selectedRecord]);
+
+    // handling input changes
+    con
+}
