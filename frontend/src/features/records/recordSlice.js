f40f5b95b1ff7f4067d6a9b477c8d1fb741b3e5c
Lisa Alusa <sayilisaalusa@gmail.com>
2025-07-18 17:20:28 +0300

    editing an existing record by matching ID

--- frontend/src/features/records/recordSlice.js
+++ frontend/src/features/records/recordSlice.js
@@ -1,5 +1,32 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const initialState = {
-    records:
-}
+    records: [], // Array to hold records
+};
+
+const recordSlice = createSlice({
+    name:'records',
+    initialState,
+    reducers: {
+        // Adding a new record
+        addRecord: {
+            reducer: (state, action)=> {
+                state.records.push(action.payload);
+            },
+            prepare: (recordData) => ({
+                payload: {
+                    id: nanoid(),
+                    ...recordData,
+                },
+            }),
+        },
+
+        updateRecord: (state, action) => {
+            const indexToUpdate = state.records.findIndex(
+                (record) => record.id === action.payload.id
+            );
+            if 
+        }
+
+    }
+})
