f2b11733436ea6fef56b313ef828eade13b76a40
Lisa Alusa <sayilisaalusa@gmail.com>
2025-07-18 17:30:35 +0300

    working on the recordForm

---
+++ frontend/src/components/RecordForm.jsx
@@ -0,0 +1,4 @@
+import Rect, { useState, useEffect } from 'react';
+import { useDispatch, useSelector} from 'react-redux';
+import { addRecord, updateRecord} from '../features/records/recordSlice';
+
